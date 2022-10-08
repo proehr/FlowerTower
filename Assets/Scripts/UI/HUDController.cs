f6ec2ffea3d776ba80bf69ad28da855c98cfb7f5
proehr <pauline.roehr@yahoo.de>
2022-10-08 14:28:55 +0200

    47-td-enemy-movement: Add nav mesh and hud ui controller

---
+++ Assets/Scripts/UI/HUDController.cs
@@ -0,0 +1,17 @@
+﻿using DataStructures.Events;
+using TowerDefense.Level;
+using UnityEngine;
+
+namespace UI
+{
+    public class HUDController : MonoBehaviour
+    {
+        public void StartWaveButtonPressed()
+        {
+            if (LevelManager.instanceExists)
+            {
+                LevelManager.instance.BuildingCompleted();
+            }
+        }
+    }
+}
