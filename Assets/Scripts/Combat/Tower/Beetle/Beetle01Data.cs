f437b00d7bc464d074c583f2eb6c1e2b87cbf1d3
proehr <pauline.roehr@yahoo.de>
2023-04-15 15:07:46 +0200

    74-combat-code-refactoring: remove old combat code folder

--- Assets/Scripts/Combat/Tower/Beetle/Beetle01Data.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace FlowerTower {
-    [CreateAssetMenu(fileName = "Beetle01Data", menuName = "Towers/Beetle01Data")]
-    public class Beetle01Data : TowerCombatData
-    {
-    }
-}
